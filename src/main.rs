6c2b774ab603bcdae97d9c973039a3241976c647
towelsh <tea@towel.sh>
2025-12-02 21:43:34 +0000

    stupid

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, rt::net::TcpStream};
-use std::env;
+use std::io::{Read, Write};
+use std::net::TcpStream;
 
-#[get("/")]
-async fn root(body: String) -> impl Responder {
-    // stream body to server
-    let Ok(stream) = TcpStream::connect("127.0.0.1:1337").await else {
-        return HttpResponse::InternalServerError();
-    };
-    if let Err(why) = stream.writable().await {
-        println!("failed to wait for writable: {:?}", why);
-        return HttpResponse::InternalServerError();
-    }
-    if let Err(why) = stream.try_write(body.as_bytes()) {
-        println!("failed to send string: {:?}", why);
-        return HttpResponse::InternalServerError();
-    }
-    if let Err(why) = stream.try_write(&[0u8]) {
-        println!("failed to send terminator: {:?}", why);
-        return HttpResponse::InternalServerError();
-    }
+use actix_web::web::{BufMut, Bytes, BytesMut};
+use actix_web::{App, HttpResponse, HttpServer, Responder, post};
+
+#[post("/")]
+async fn root(bytes: Bytes) -> impl Responder {
+    println!("body: {:?}", bytes);
+
+    let mut stream = TcpStream::connect("127.0.0.1:1337").unwrap();
+
+    let mut m = BytesMut::with_capacity(bytes.len() + 1);
+    m.extend_from_slice(&bytes);
+    m.put_u8(0x00);
+    stream.write_all(&m).unwrap();
+
+    let mut buf = [0u8; 1024];
+    let n = stream.read(&mut buf).unwrap();
+    println!("got {} bytes: {:?}", n, &buf[..n]);
+
     HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(root))
-        .bind((
-            env::var("HTTP_ADDRESS").unwrap_or("127.0.0.1".into()),
-            env::var("HTTP_PORT")
-                .unwrap_or("8080".into())
-                .parse()
-                .unwrap_or(8080),
-        ))?
+        .bind(("127.0.0.1", 8081))?
         .run()
         .await
 }
